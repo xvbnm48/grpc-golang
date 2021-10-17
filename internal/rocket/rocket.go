2c0861fa706b7721e6798899dc68add52e8f7c90
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2021-10-17 22:55:01 +0700

    lanjut buat konfigurasi interface store di rocket dan memperbarui readme

--- internal/rocket/rocket.go
+++ internal/rocket/rocket.go
@@ -9,10 +9,21 @@ type Rocket struct {
 	Flights int
 }
 
+// store - defines the interface we expect
+//our database the rocket inventory
+type Store interface {
+	GetRocketByID(id string) (Rocket, error)
+	InsertRocket(rkt Rocket) (Rocket, error)
+	DeleteRocket(id string) error
+}
+
 // Service - our rocket service , responsible for
 // updating the rocket inventory
 type Service struct{}
 
-func New() Service {
-	return Service{}
+//New return a new instance of our rocket service
+func New(store Store) Service {
+	return Service{
+		Store: store,
+	}
 }
