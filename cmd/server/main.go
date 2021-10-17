82710f246ad5fd1bd0c02f4b99eac3c9257bc932
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2021-10-17 22:24:23 +0700

    grpcc

---
+++ cmd/server/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "log"
+
+func Run() error {
+	//responsible for initializing and starting
+	// out grpc
+	return nil
+}
+
+func main() {
+	if err := Run(); err != nil {
+		log.Fatal(err)
+	}
+}
