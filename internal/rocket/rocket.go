d2826abe2735abb7898e0870c4a48bae629ff78e
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2021-10-19 23:25:16 +0700

    membuat testing dengan mock

--- internal/rocket/rocket.go
+++ internal/rocket/rocket.go
@@ -1,4 +1,8 @@
+//go:generate mockgen -destination=rocket_mocks_test.go -package=rocket github.com/xvbnm48/grpc-golang/internal/rocket Store
+
 package rocket
+
+import "context"
 
 //Rocket - should of countain the definition on our
 //rocket
@@ -19,11 +23,44 @@ type Store interface {
 
 // Service - our rocket service , responsible for
 // updating the rocket inventory
-type Service struct{}
+type Service struct {
+	Store Store
+}
 
 //New return a new instance of our rocket service
 func New(store Store) Service {
 	return Service{
 		Store: store,
 	}
+}
+
+//get rocket by id- retrives a rocket based on the id the store
+func (s Service) GetRocketByID(ctx context.Context, id string) (Rocket, error) {
+	rkt, err := s.Store.GetRocketByID(id)
+	if err != nil {
+		return Rocket{}, err
+	}
+	return rkt, nil
+
+}
+
+//inser rocket - insert a new rocket to store
+func (s Service) InsertRocket(ctx context.Context, rkt Rocket) (Rocket, error) {
+	rkt, err := s.Store.InsertRocket(rkt)
+
+	if err != nil {
+		return Rocket{}, err
+	}
+
+	return rkt, nil
+}
+
+//delete rocket - deletes a rocket from our inventory
+func (s Service) DeleteRocket(id string) error {
+	err := s.Store.DeleteRocket(id)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
